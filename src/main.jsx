89df7f551800353d5844674fd5e750b6ad6eee09
Pluck3dEye <89025539+Pluck3dEye@users.noreply.github.com>
2025-05-24 20:02:34 +0700

    Remove Strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import { AuthProvider } from './authContext';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React>
 );
